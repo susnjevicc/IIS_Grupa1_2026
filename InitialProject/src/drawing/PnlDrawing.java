f5cfa06f9ad5602ed63bd039985bbb94987250fc
User <User@DESKTOP-0VNO67T>
2026-08-17 22:06:03 +0200

    Kreiranje JFrame-a i ostalih klasa u drawing-u

---
+++ InitialProject/src/drawing/PnlDrawing.java
@@ -0,0 +1,16 @@
+package drawing;
+
+import javax.swing.JPanel;
+
+public class PnlDrawing extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public PnlDrawing() {
+
+	}
+
+}
